e87f7e0e57b3d1dafedbbdd3c6459521cc2fee7e
patthoyts <patthoyts@users.sourceforge.net>
2003-03-08 15:45:56 +0000

    Added bindings for Return and Escape to the login dialog.

--- apps/tkchat/tkchat.tcl
+++ apps/tkchat/tkchat.tcl
@@ -37,7 +37,7 @@ if {![catch {package vcompare $tk_patchLevel $tk_patchLevel}]} {
 
 package forget app-tkchat	;# Workaround until I can convince people
 				;# that apps are not packages.  :)  DGP
-package provide app-tkchat [regexp -inline {\d+\.\d+} {$Revision: 1.84 $}]
+package provide app-tkchat [regexp -inline {\d+\.\d+} {$Revision: 1.85 $}]
 
 namespace eval ::tkchat {
     # Everything will eventually be namespaced
@@ -48,7 +48,7 @@ namespace eval ::tkchat {
     variable HOST http://purl.org/mini
 
     variable HEADUrl {http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/tcllib/tclapps/apps/tkchat/tkchat.tcl?rev=HEAD}
-    variable rcsid   {$Id: tkchat.tcl,v 1.84 2003/03/08 10:59:23 patthoyts Exp $}
+    variable rcsid   {$Id: tkchat.tcl,v 1.85 2003/03/08 15:45:56 patthoyts Exp $}
 
     variable MSGS
     set MSGS(entered) [list \
@@ -1963,6 +1963,8 @@ proc ::tkchat::logonScreen {} {
 	grid x .logon.atc  - -sticky w -pady 3
 	grid .logon.ok - .logon.cn -pady 10
 	wm resizable .logon 0 0
+        bind .logon <Return> [list .logon.ok invoke]
+        bind .logon <Escape> [list .logon.cn invoke]
     }
     optSet
     catch {::tk::PlaceWindow .logon widget .}
